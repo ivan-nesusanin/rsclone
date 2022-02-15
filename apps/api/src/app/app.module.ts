e5dd9aae7f528cbde5f290e32aa706914b517f38
Anton Yashin <antoshkoo@gmail.com>
2022-02-15 13:10:59 +0300

    feat: add user

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { MovieModule } from './movie/movie.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MovieModule,
+    UserModule,
     MongooseModule.forRoot(
-      `mongodb+srv://rsstudent:FJLjwzI0gaKzGEok@cluster0.w9p60.mongodb.net/movies?retryWrites=true&w=majority`
+      `mongodb+srv://rsstudent:FJLjwzI0gaKzGEok@cluster0.w9p60.mongodb.net/rsclone?retryWrites=true&w=majority`
     ),
   ],
 })
--- libs/models/src/lib/models.module.ts
+++ libs/models/src/lib/models.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 export { Authenticate } from './Authenticate';
 export { /* Movie, MovieResponse, */ MovieId } from './Movie';
+export { UserModel } from './User';
 
 @NgModule({
   imports: [CommonModule],
