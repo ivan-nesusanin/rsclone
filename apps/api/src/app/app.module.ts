ace8f9fd7375c24d1546db0dd5a55637d3f9f913
Maksim Perevyazkin <kvadratpm@gmail.com>
2022-02-23 12:24:35 +0300

    try1

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
@@ -18,10 +16,10 @@ import { join } from 'path';
     MongooseModule.forRoot(
       `mongodb+srv://rsstudent:FJLjwzI0gaKzGEok@cluster0.w9p60.mongodb.net/movies?retryWrites=true&w=majority`
     ),
-    ServeStaticModule.forRoot({
+    /* ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'frontend'),
       exclude: ['/api*'],
-    }),
+    }), */
   ],
   controllers: [AppController],
 })
